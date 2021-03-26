6832fbe1a8a5af480e69fe17b745dda9744fa8fb
Nicolas Berthou <berthou.nicolas@laposte.net>
2021-03-27 00:12:33 +0100

    CLEANED repo + ADD homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import HomePage from './Pages/HomePage';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Mukta&display=swap');
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  font-family: "Mukta", "Arial", sans-serif;
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <HomePage />
     </div>
   );
 }
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,175 @@
+import styled from 'styled-components';
+import ReactParticles from 'react-tsparticles';
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: #2b2d2f;
+    color: white;
+`
+
+const TitleContainer = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+const Title = styled.h1`
+    margin: 0;
+    font-size: 3em;
+`;
+
+const Subtitle = styled.h2`
+    margin: 0;
+    font-size: 2.5em;
+`;
+
+const params = {
+    "particles": {
+      "number": {
+        "value": 50,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 3
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 7.6,
+        "direction": "top-right",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+const HomePage = () => (
+    <Container>
+        <Particles>
+        <Title>NICOLAS BERTHOU</Title>
+        <Subtitle>Développeur web</Subtitle>
+        <br/>
+        <b>Coming soon<sup>TM</sup></b>
+       </Particles>
+    </Container>
+);
+
+type ParticlesProps = {
+    children: any;
+}
+
+const Particles = ({children }: ParticlesProps) => (
+    <div style={{position: 'relative', height: '100vh'}}>
+    <ReactParticles
+        params={params}
+        style={{
+            position: 'absolute',
+            zIndex: 1,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+        }}
+    />
+    {children && <TitleContainer style={{position: "relative"}}>{children}</TitleContainer>}
+    </div>
+)
+
+export default HomePage;
